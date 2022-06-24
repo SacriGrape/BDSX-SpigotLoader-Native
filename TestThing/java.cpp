da5e88173b1f61966ad28aed49caac176a4d193b
Evan Lee <evan6lee@gmail.com>
2022-06-24 06:42:14 -0500

    Fixed useless line

--- TestThing/java.cpp
+++ TestThing/java.cpp
@@ -5,8 +5,6 @@
 
 void initJava()
 {
-	SetDllDirectoryA("../SpigotLoader/jre/bin");
-
 	JavaVM* jvm;
 	JNIEnv* env;
 	JavaVMInitArgs vm_args;
@@ -14,7 +12,7 @@ void initJava()
 	vm_args.version = JNI_VERSION_10;
 	vm_args.nOptions = 0;
 	vm_args.ignoreUnrecognized = false;
-	JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args);
+	std::cout << JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args) << "\n";
 	
 	jclass urlClass = env->FindClass("java/net/URL");
 	jmethodID urlConstructor = env->GetMethodID(urlClass, "<init>", "(Ljava/lang/String;)Ljava/net/URL;");
